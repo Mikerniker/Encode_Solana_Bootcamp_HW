c59009522880fb541d8e64ee795cb3aaed34d3fe
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-03-30 02:37:47 +0800

    Add day4 hw fizzbuzz initial file

--- Week1_Intro_to_Solana_Blockchain_Rust/Day4/bootcamp/src/main.rs
+++ Week1_Intro_to_Solana_Blockchain_Rust/Day4/bootcamp/src/main.rs
@@ -3,6 +3,13 @@ fn main() {
 
     
     for num in 1..=301 {
-        println!("{}", num);
-    }
+        match (num % 3, num % 5) {
+            (0, 0) => println!("Fizz Buzz"),
+            (0, _) => println!("Fizz"),
+            (_, 0) => println!("Buzz"),
+            _ => println!("{}", num),
+        }
+    }  // to do count number of time fizz buzz occcured 
+
+   
 }
