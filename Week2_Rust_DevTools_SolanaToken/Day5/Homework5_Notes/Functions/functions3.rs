39b0d122ad0d5b48b23443dd030e9fac48c169f8
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-02 02:37:50 +0800

    Add Day5 hw for functions 3

---
+++ Week2_Rust_DevTools_SolanaToken/Day5/Homework5_Notes/Functions/functions3.rs
@@ -0,0 +1,13 @@
+// functions3.rs
+// Make me compile! Execute `rustlings hint functions3` for hints :)
+//Add an argument to call_this e.g. from call_this()  to call_this(5)
+
+fn main() {
+    call_this(5);
+}
+
+fn call_this(num: u32) {
+    for i in 0..num {
+        println!("Loop now {}", i + 1);
+    }
+}
