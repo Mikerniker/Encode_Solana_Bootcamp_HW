8f6347b07cd1fde780bad3442120b7c54bda6857
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-02 01:54:06 +0800

    Update Day4 hw

--- Week1_Intro_to_Solana_Blockchain_Rust/Day4/bootcamp/src/main.rs
+++ Week1_Intro_to_Solana_Blockchain_Rust/Day4/bootcamp/src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
     println!("Welcome to FizzBuzz!");
 
-    
+    let mut fizz_buzz_count = 0;
     for num in 1..=301 {
         match (num % 3, num % 5) {
-            (0, 0) => println!("Fizz Buzz"),
+            (0, 0) => {println!("Fizz Buzz"); fizz_buzz_count += 1;},
             (0, _) => println!("Fizz"),
             (_, 0) => println!("Buzz"),
             _ => println!("{}", num),
         }
-    }  // to do count number of time fizz buzz occcured 
-
+    }
+    println!("Total 'Fizz Buzz': {fizz_buzz_count}")
    
 }
