28d4e10b3d884a0242ddcbf8f9555c0d79337f40
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-02 02:33:55 +0800

    Add Day5 hw for functions 1

---
+++ Week2_Rust_DevTools_SolanaToken/Day5/Homework5_Notes/Functions/functions1.rs
@@ -0,0 +1,11 @@
+// functions1.rs
+// Make me compile! Execute `rustlings hint functions1` for hints :)
+// Added call_me function
+
+
+fn main() {
+    call_me();
+}
+fn call_me() {
+    println!("Call me.");
+}
