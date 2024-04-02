3f30dea161519bb329f9c60b628cffe1f170100e
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-03 02:52:01 +0800

    Add Day5 primitive types 3 exercise

---
+++ Week2_Rust_DevTools_SolanaToken/Day5/Homework5_Notes/PrimitiveTypes/primitive_types3.rs
@@ -0,0 +1,13 @@
+// primitive_types3.rs
+// Create an array with at least 100 elements in it where the ??? is.
+//  Added    let a =  [5; 100];
+
+fn main() {
+    let a =  [5; 100];
+
+    if a.len() >= 100 {
+        println!("Wow, that's a big array!");
+    } else {
+        println!("Meh, I eat arrays like that for breakfast.");
+    }
+}
