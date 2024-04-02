007f82dc64fbb12537b4e18889073d75364ab68d
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-03 01:58:10 +0800

    Add Day5 Variables6 exercise

---
+++ Week2_Rust_DevTools_SolanaToken/Day5/Homework5_Notes/Variables/variables6.rs
@@ -0,0 +1,8 @@
+// variables6.rs
+// NOTES: assign a type to the const, ie. const NUMBER = 3; to const NUMBER: u32 = 3;
+
+
+const NUMBER: u32 = 3;
+fn main() {
+    println!("Number {}", NUMBER);
+}
