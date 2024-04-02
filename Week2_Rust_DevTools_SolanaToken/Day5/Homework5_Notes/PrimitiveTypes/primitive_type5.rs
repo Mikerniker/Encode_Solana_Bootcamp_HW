9d0bda2b98e3588d954280a9ae00b76c31157038
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-03 02:59:06 +0800

    Add Day5 primitive types 5 exercise

---
+++ Week2_Rust_DevTools_SolanaToken/Day5/Homework5_Notes/PrimitiveTypes/primitive_type5.rs
@@ -0,0 +1,11 @@
+// primitive_types5.rs
+// Destructure the `cat` tuple so that the println will work.
+// Added  let (name, age) = cat;
+
+
+fn main() {
+    let cat = ("Furry McFurson", 3.5);
+    let (name, age) = cat;
+
+    println!("{} is {} years old.", name, age);
+}
