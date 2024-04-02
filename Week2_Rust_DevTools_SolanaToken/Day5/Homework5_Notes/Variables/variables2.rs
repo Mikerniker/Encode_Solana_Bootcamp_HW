f7f5fc814dd67a3dfbde346914efec4d7105989d
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-03 01:46:38 +0800

    Add Day5 Variables2 exercise

---
+++ Week2_Rust_DevTools_SolanaToken/Day5/Homework5_Notes/Variables/variables2.rs
@@ -0,0 +1,12 @@
+// variables2.rs
+// NOTES change let x  to let x = 10;
+
+
+fn main() {
+    let x = 10;
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
