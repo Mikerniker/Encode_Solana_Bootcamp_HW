121c61876a4c614c1903ac8f5fe06df60fd1b338
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-03 01:50:26 +0800

    Add Day5 Variables4 exercise

---
+++ Week2_Rust_DevTools_SolanaToken/Day5/Homework5_Notes/Variables/variables4.rs
@@ -0,0 +1,10 @@
+// variables4.rs
+// NOTE: make x mutable, ie. let x = 3; to let mut x = 3;
+
+
+fn main() {
+    let mut x = 3;
+    println!("Number {}", x);
+    x = 5; // don't change this line
+    println!("Number {}", x);
+}
