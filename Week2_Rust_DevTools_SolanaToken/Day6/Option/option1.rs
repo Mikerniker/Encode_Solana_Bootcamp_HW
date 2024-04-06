e6766c71a6bc4ca7b382396d11daa84851977d38
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2024-04-07 02:41:29 +0800

    Add option1 exercise

---
+++ Week2_Rust_DevTools_SolanaToken/Day6/Option/option1.rs
@@ -0,0 +1,48 @@
+// option1.rs
+// Make me compile! Execute `rustlings hint option1` for hints
+//
+
+
+// you can modify anything EXCEPT for this function's signature
+
+fn print_number(maybe_number: Option<u16>) {
+    println!("printing: {}", maybe_number.unwrap());
+}
+
+fn main() {
+    print_number(Some(13));   // Add Some
+    print_number(Some(99));   // Add Some
+
+    let mut numbers: [Option<u16>; 5] = [None; 5];   // Add None; 5, not clear why
+    for iter in 0..5 {
+        let number_to_add: u16 = {
+            ((iter * 1235) + 2) / (4 * 16)
+        };
+
+        numbers[iter as usize] = Some(number_to_add);  // Add Some
+    }
+}
+
+
+// ORIGINAL
+
+// option1.rs
+
+// you can modify anything EXCEPT for this function's signature
+//fn print_number(maybe_number: Option<u16>) {
+//    println!("printing: {}", maybe_number.unwrap());
+//}
+
+//fn main() {
+//    print_number(13);
+//    print_number(99);
+
+//    let mut numbers: [Option<u16>; 5];
+//    for iter in 0..5 {
+//        let number_to_add: u16 = {
+//            ((iter * 1235) + 2) / (4 * 16)
+//        };
+
+//        numbers[iter as usize] = number_to_add;
+//    }
+//}
